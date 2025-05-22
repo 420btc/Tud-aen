58121a8bb36282004819db5605635bb3766deb30
420btc <chocolati_558@hotmail.com>
2025-05-22 18:09:50 +0200

    feat: add root layout with metadata and favicon configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,14 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'TuDíaEn...',
+  title: 'YourDayIn',
   description: 'TuDíaEn... es una aplicación que te ayuda a planificar tu día en cualquier lugar.',
   generator: 'freire',
+  icons: {
+    icon: '/myfavi.png',
+    shortcut: '/myfavi.png',
+    apple: '/myfavi.png',
+  },
 }
 
 export default function RootLayout({
